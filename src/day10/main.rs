0258c24b22ac3b213a1bb8f488732f8289bfce6a
oskar117 <olek.konieczny@wp.pl>
2025-12-26 20:14:39 +0100

    day10 part2 that is still too slow

--- src/day10/main.rs
+++ src/day10/main.rs
@@ -1,4 +1,4 @@
-use std::collections::VecDeque;
+use std::collections::{HashSet, VecDeque};
 use std::fs;
 
 fn main() {
@@ -107,34 +107,102 @@ fn part2(filename: &str) -> i32 {
     let mut result = 0;
     for (index, target_joltage) in target_joltages.iter().enumerate() {
         println!("processing {:?}, current res = {}", target_joltage, result);
-        let mut queue = VecDeque::from(
-            buttons[index]
-                .clone()
-                .iter()
-                .map(|btn| (vec![0; target_joltage.len()], btn.clone(), 0))
-                .collect::<Vec<(Vec<i32>, Vec<usize>, i32)>>()
-        );
-
-        loop {
-            let (processed_joltage, button_to_press, press_count) = queue.pop_front().unwrap();
-            let mut new_joltage = processed_joltage.clone();
-            for btn_index in button_to_press.iter() {
-                new_joltage[*btn_index] += 1;
-            }
-            if new_joltage == *target_joltage {
-                result += press_count + 1;
-                break;
-            }
-            if new_joltage.iter().enumerate().any(|(idx, elemeent)| elemeent > &target_joltage[idx]) {
-                continue
-            }
-            buttons[index]
-                .clone()
-                .iter()
-                .map(|btn| (new_joltage.clone(), btn.clone(), press_count + 1))
-                .for_each(|el| queue.push_back(el))
-        }
+        result += process_joltage(&target_joltage, &buttons[index]);
     }
 
     result
+}
+
+fn process_joltage(target_joltage: &Vec<i32>, buttons: &Vec<Vec<usize>>) -> i32 {
+    let mut cache = HashSet::<(Vec<i32>, &Vec<usize>, i32)>::new();
+
+    let mut queue = VecDeque::from(reduce_possible_presses(&mut cache, &target_joltage, &vec![0; target_joltage.len()], 0, &buttons));
+    while queue.len() > 0 {
+        let queue_message = queue.pop_front().unwrap();
+        if cache.contains(&queue_message) {
+            continue;
+        }
+        cache.insert(queue_message.clone());
+        let (processed_joltage, button_to_press, press_count) = queue_message;
+        if processed_joltage == *target_joltage {
+            println!("got result from {:?} = {}", target_joltage, press_count);
+            return press_count;
+        }
+        let mut new_joltage = processed_joltage.clone();
+        for btn_index in button_to_press.iter() {
+            new_joltage[*btn_index] += 1;
+        }
+        if new_joltage == *target_joltage {
+            println!("got result from {:?} = {}", target_joltage, press_count + 1);
+            return press_count + 1;
+        }
+        if new_joltage.iter().enumerate().any(|(idx, elemeent)| elemeent > &target_joltage[idx]) {
+            continue
+        }
+
+        let new_entries = reduce_possible_presses(&mut cache, &target_joltage, &new_joltage, press_count + 1, &buttons);
+        new_entries.iter()
+            .filter(|element| !cache.contains(element))
+            .for_each(|element| queue.push_back(element.clone()));
+    }
+    0
+}
+
+fn reduce_possible_presses<'a>(cache: &mut HashSet<(Vec<i32>, &'a Vec<usize>, i32)>, target_joltage: &Vec<i32>, current_joltage: &Vec<i32>, current_press: i32, buttons: &'a Vec<Vec<usize>>) -> Vec<(Vec<i32>, &'a Vec<usize>, i32)> {
+    // println!("reduc {:?} {}", current_joltage, current_press);
+    let min_press_joltage = target_joltage.iter()
+        .enumerate()
+        .map(|(i, val)| (i, val, buttons.iter().filter(|btn| btn.contains(&i)).count()))
+        .filter(|(i, val, _)| current_joltage[*i] < **val)
+        .min_by_key(|(_,_,count)| *count)
+        .unwrap();
+    // println!("{:?} {:?}", min_press_joltage, current_joltage);
+    let affected_buttons = buttons.iter().filter(|btn| btn.contains(&min_press_joltage.0) && btn.iter().all(|&idx| current_joltage[idx] < target_joltage[idx])).collect::<Vec<&Vec<usize>>>();
+    let not_affected_buttons = buttons.iter().filter(|btn| !btn.contains(&min_press_joltage.0)).collect::<Vec<&Vec<usize>>>();
+    let mut reduction_queue = VecDeque::from(affected_buttons.iter()
+        .map(|&btn| (current_joltage.clone(), btn, current_press))
+        .collect::<Vec<(Vec<i32>, &Vec<usize>, i32)>>()
+    );
+    let mut queue = Vec::<(Vec<i32>, &Vec<usize>, i32)>::new();
+    while reduction_queue.len() > 0 {
+        let queue_message = reduction_queue.pop_front().unwrap();
+        if cache.contains(&queue_message) {
+            continue;
+        }
+        cache.insert(queue_message.clone());
+        let (processed_joltage, button_to_press, press_count) = queue_message;
+        if processed_joltage[min_press_joltage.0] == target_joltage[min_press_joltage.0] {
+            buttons
+                .iter()
+                .filter(|btn| btn.iter().filter(|idx| **idx != min_press_joltage.0).all(|&idx| processed_joltage[idx] <= target_joltage[idx]))
+                .map(|btn| (processed_joltage.clone(), btn, press_count))
+                .filter(|element| !cache.contains(element))
+                .for_each(|el| queue.push(el));
+            continue;
+        }
+        let mut new_joltage = processed_joltage.clone();
+        for btn_index in button_to_press.iter() {
+            new_joltage[*btn_index] += 1;
+        }
+        if new_joltage.iter().enumerate().any(|(idx, elemeent)| elemeent > &target_joltage[idx]) {
+            continue
+        }
+        if new_joltage[min_press_joltage.0] == target_joltage[min_press_joltage.0] {
+            buttons
+                .iter()
+                .filter(|btn| btn.iter().filter(|idx| **idx != min_press_joltage.0).all(|&idx| new_joltage[idx] <= target_joltage[idx]))
+                .map(|btn| (new_joltage.clone(), btn, press_count + 1))
+                .filter(|element| !cache.contains(element))
+                .for_each(|el| queue.push(el));
+            continue;
+        }
+        affected_buttons
+            .iter()
+            .filter(|btn| btn.iter().all(|&idx| new_joltage[idx] < target_joltage[idx]))
+            .map(|&btn| (new_joltage.clone(), btn, press_count + 1))
+            .filter(|element| !cache.contains(element))
+            .for_each(|el| reduction_queue.push_back(el))
+    }
+    // println!("{:?}", queue);
+    queue
 }
